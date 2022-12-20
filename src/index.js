4eeb6b47ed8921e18993d0965413fcbf8000ee8d
ZahrizhalAli <zzahrizhal.ali@gmail.com>
2022-12-20 22:34:06 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root, { loader as rootLoader, action as rootAction } from './Root.js';
+import ErrorPage from './error-page.js';
+import Contact, {
+  loader as contactLoader,
+  action as deleteAction,
+} from './routes/Contact';
+import EditContact, { action as editAction } from './routes/EditContact';
+import App from './routes/App';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: 'contacts/:contactId/destroy',
+        loader: contactLoader,
+        action: deleteAction,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/routes/Second.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Second() {
+  return <h1>test</h1>;
+}
+
+export default Second;
